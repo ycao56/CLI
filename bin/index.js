2fdd47c2741b948ce01f16a86e7dd9ba5d17e309
Alex Tran <alex.tran1502@gmail.com>
2022-08-03 20:02:42 -0500

    Fixed module import

--- bin/index.js
+++ bin/index.js
@@ -1,20 +1,19 @@
 #! /usr/bin/env node
-import { program, Option } from "commander";
-import fs from "fs";
-import { fdir } from "fdir";
-import si from "systeminformation";
-import readline from "readline";
-import path from "path";
-import FormData from "form-data";
-import cliProgress from "cli-progress";
-import { stat, readFile } from "fs/promises";
-import exifr from "exifr";
-import axios from "axios";
-import pjson from "../package.json" assert { type: "json" };
-
+const { default: axios } = require("axios");
+const { program, Option } = require("commander");
+const fs = require("fs");
+const { fdir } = require("fdir");
+const si = require("systeminformation");
+const readline = require("readline");
+const path = require("path");
+const FormData = require("form-data");
+const cliProgress = require("cli-progress");
+const { stat } = require("fs/promises");
+const exifr = require("exifr");
+var pjson = require("../package.json");
 // GLOBAL
-import mime from "mime-types";
-import chalk from "chalk";
+const mime = require("mime-types");
+const chalk = require("chalk");
 const log = console.log;
 const rl = readline.createInterface({
   input: process.stdin,
